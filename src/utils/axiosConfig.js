bb89d2b8d90d60e221caad05f03dffe79c482c73
afeDayo <afedayo@gmail.com>
2024-09-12 16:18:24 +0100

    Last commit

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://entertainment-app-server-4pq5.onrender.com",
+  baseURL: "https://my-movies-backend-0r43.onrender.com",
 });
 
 // Adding a response interceptor
